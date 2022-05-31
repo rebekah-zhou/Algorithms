401d2946b1d8bfb446d59db09b79abac1986c8be
Rebekah Zhou <rebekahzhou@gmail.com>
2022-05-31 13:56:27 -0700

    add popMin method

--- Data Structures/Heap/MinHeap.js
+++ Data Structures/Heap/MinHeap.js
@@ -13,15 +13,27 @@ class MinHeap {
     }
   
     bubbleUp() {
-      let current = this.size
-      while(current > 1 && this.heap[current] < this.heap[getParent(current)]) {
-        this.swap(current, getParent(current))
-        current = getParent(current)
+      let current = this.size;
+      while (current > 1 && this.heap[getParent(current)] > this.heap[current]) {
+        console.log('..', this.heap);
+        console.log(`.. swap index ${current} with ${getParent(current)}`);
+        this.swap(current, getParent(current));
+        current = getParent(current);
       }
     }
   
     swap(a, b) {
       [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+    }
+  
+    popMin() {
+      if (this.size < 1) return null
+      console.log(`Swapping the first element ${this.heap[1]} with the last element, ${this.heap[this.size]}`)
+      this.swap(this.size, 1)
+      const min = this.heap.pop()
+      console.log(`Remove ${min} to get ${this.heap}`)
+      this.size--
+      return min
     }
   
   }
