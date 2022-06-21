0d435714063683230ea6228e2aa80284ae697ecf
Rebekah Zhou <68443819+rebekah-zhou@users.noreply.github.com>
2022-06-21 15:11:29 -0700

    Time: 101 ms (30.54%), Space: 43.1 MB (36.71%) - LeetHub

---
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    // two pointers
+    // start first pointer, with a counter
+    // once the counter === n, set the second pointer = head.
+    // once the first pointer == null, remove the node at the second pointer
+    
+    if (head.next === null && n === 1) return null
+    
+    let pointer1 = head
+    let pointer2
+    let count = 1
+    
+    while(pointer1 !== null) {
+        if (count === n + 1) {
+            pointer2 = head
+        } else if (count > n) {
+            pointer2 = pointer2.next     
+        } 
+        pointer1 = pointer1.next
+        count++
+    }
+    
+    if (pointer2) {
+        const nodeToRemove = pointer2.next 
+        pointer2.next = pointer2.next.next 
+        nodeToRemove.next = null
+    } else {
+        head = head.next
+    }
+    
+    
+    return head
+};
